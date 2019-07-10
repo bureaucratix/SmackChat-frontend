72ae67c8e49194be7e33b75bbe833c0068e71d66
jkshareef <jamalkshareef@gmail.com>
2019-07-10 09:08:35 -0700

    attempt to fix user update on backend

--- channel-chat/src/components/UserPopUp.js
+++ channel-chat/src/components/UserPopUp.js
@@ -1,10 +1,13 @@
-import React, {Component} from 'react'
-import { Button, Header, Image, Modal } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button, Modal, Header, Image} from 'semantic-ui-react'
 
-export default class UserPopUp extends Component {
+class UserPopUp extends Component {
+  state = { 
+    open: false,
+    user: '' }
   
 
-  componentDidMount() {
+    componentDidMount() {
     this.fetchUser()
   }
 
@@ -12,9 +15,7 @@ export default class UserPopUp extends Component {
     return localStorage.getItem('jwt')
   }
 
-  
-
-  fetchUser = () => {
+    fetchUser = () => {
     let token = this.getToken()
     fetch('http://localhost:3000/api/v1/profile', {
       headers: {
@@ -23,65 +24,76 @@ export default class UserPopUp extends Component {
     })
     .then(resp => resp.json())
     .then(data => {
-      console.log('prevState:', this.state)
-      this.setState({ user: data.user})
-    }, () => console.log('state:', this.state))
+      console.log('prevState', this.state)
+      this.setState({
+        user: data.user
+      }, () => console.log('state', this.state))
+      
+    })
   }
 
-  
-
-  
-  
-handleSubmit = (img) => {
-  let token = this.getToken()
-  let payload = img
-  fetch('http://localhost:3000/api/v1/profile)', {
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const token = this.getToken();
+    const payload = {user: this.state.user};
+    const config = {
       method: 'PUT',
-      headers: {
-          'Authorization': 'Bearer ' + token
-      },
+      headers: { 'Authorization': 'Bearer ' + token},
       body: JSON.stringify(payload)
-  })
-  .then(resp => resp.json())
-  .then(data => {
-    this.setState({
-      user: {
-        ...this.state.user,
-        img_url: data
-      }
+    };
+    fetch(`http://localhost:3000/api/v1/users/${this.state.user.id}`, config)
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
     })
-  })
-}
+  }
+
 
   handleChange = (event) => {
     this.setState({
-      img_url: event.target.value
+      user: {
+        ...this.state.user,
+        img_url: event.target.value
+      }
     })
   }
 
 
-
+  show = size => () => this.setState({ size, open: true })
+  close = () => this.setState({ open: false })
 
   render() {
+    const { open, size } = this.state
 
-    const ModalModalExample = () => (
-      <Modal trigger={<Button>Show Modal</Button>}>
-        <Modal.Header>My Account</Modal.Header>
-        <Modal.Content image>
-          <Image wrapped size='medium' src={this.state.user.ing_url} />
-          <Modal.Description>
-            <Header>Default Profile Image</Header>
-            <p>We've found the following gravatar image associated with your e-mail address.</p>
-            <p>Is it okay to use this photo?</p>
-          </Modal.Description>
-        </Modal.Content>
-      </Modal>
-    )
-    
     return (
-      {ModalModalExample}
+      <div>
+        
+        <Button onClick={this.show('fullscreen')}>Profile</Button>
+
+        <Modal size={size} open={open} onClose={this.close}>
+          <Modal.Header>My Profile</Modal.Header>
+          <Modal.Content image>
+            <Image wrapped size='medium' src={this.state.user.img_url} />
+            <Modal.Description>
+              <Header>Default Profile Image</Header>
+              <p>We've found the following gravatar image associated with your e-mail address.</p>
+              <p>Is it okay to use this photo?</p>
+              <form onSubmit={this.handleSubmit}>
+              <label>
+                Add or Edit Profile Image:
+                <input type="text" value={this.state.user.img_url} onChange={this.handleChange}/>
+              </label>
+            </form>
+            </Modal.Description>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button negative>No</Button>
+            <Button positive icon='checkmark' labelPosition='right' content='Yes' />
+          </Modal.Actions>
+        </Modal>
+      </div>
     )
   }
 }
-  
 
+export default UserPopUp
